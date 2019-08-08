86a3371df4d3da03c2b1b99209c43ecddc984307
Bhavye Sharma <bhavye.sharma1996@gmail.com>
2019-08-09 01:44:13 +0530

    Modified theme and changed question logic isAll

--- src/components/QuizApp.js
+++ src/components/QuizApp.js
@@ -1,10 +1,10 @@
-import React, { Fragment, Component } from 'react';
-import PropTypes from 'prop-types';
-import Quiz from './Quiz';
-import Modal from './Modal';
-import Results from './Results';
-import shuffleQuestions from '../helpers/shuffleQuestions';
-import QUESTION_DATA from '../data/quiz-data';
+import React, { Fragment, Component } from "react";
+import PropTypes from "prop-types";
+import Quiz from "./Quiz";
+import Modal from "./Modal";
+import Results from "./Results";
+import shuffleQuestions from "../helpers/shuffleQuestions";
+import QUESTION_DATA from "../data/quiz-data";
 
 class QuizApp extends Component {
   state = {
@@ -25,30 +25,30 @@ class QuizApp extends Component {
       userAnswers: QUESTIONS.map(() => {
         return {
           tries: 0
-        }
+        };
       }),
       step: 0,
       score: 0,
       modal: {
-        state: 'hide',
-        praise: '',
-        points: ''
+        state: "hide",
+        praise: "",
+        points: ""
       },
-      started:false
+      started: false
     };
   }
 
-  handleAnswerClick = (index) => (e) => {
+  handleAnswerClick = index => e => {
     const { questions, step, userAnswers } = this.state;
     const isCorrect = questions[0].correct === index;
     const currentStep = step - 1;
     const tries = userAnswers[currentStep].tries;
 
-    if (isCorrect && e.target.nodeName === 'LI') {
+    if ((isCorrect && e.target.nodeName === "LI") || questions[0].isAll) {
       // Prevent other answers from being clicked after correct answer is clicked
-      e.target.parentNode.style.pointerEvents = 'none';
+      e.target.parentNode.style.pointerEvents = "none";
 
-      e.target.classList.add('right');
+      e.target.classList.add("right");
 
       userAnswers[currentStep] = {
         tries: tries + 1
@@ -61,11 +61,9 @@ class QuizApp extends Component {
       setTimeout(() => this.showModal(tries), 750);
 
       setTimeout(this.nextStep, 2750);
-    }
-
-    else if (e.target.nodeName === 'LI') {
-      e.target.style.pointerEvents = 'none';
-      e.target.classList.add('wrong');
+    } else if (e.target.nodeName === "LI") {
+      e.target.style.pointerEvents = "none";
+      e.target.classList.add("wrong");
 
       userAnswers[currentStep] = {
         tries: tries + 1
@@ -77,41 +75,41 @@ class QuizApp extends Component {
     }
   };
 
-  handleEnterPress = (index) => (e) => {
+  handleEnterPress = index => e => {
     if (e.keyCode === 13) {
       this.handleAnswerClick(index)(e);
     }
   };
 
-  showModal = (tries) => {
+  showModal = tries => {
     let praise;
     let points;
 
     switch (tries) {
       case 0: {
-        praise = '1st Try!';
-        points = '+10';
+        praise = "1st Try!";
+        points = "+10";
         break;
       }
       case 1: {
-        praise = '2nd Try!';
-        points = '+5';
+        praise = "2nd Try!";
+        points = "+5";
         break;
       }
       case 2: {
-        praise = 'Correct!';
-        points = '+2';
+        praise = "Correct!";
+        points = "+2";
         break;
       }
       default: {
-        praise = 'Correct!';
-        points = '+1';
+        praise = "Correct!";
+        points = "+1";
       }
     }
 
     this.setState({
       modal: {
-        state: 'show',
+        state: "show",
         praise,
         points
       }
@@ -129,17 +127,21 @@ class QuizApp extends Component {
       score: this.updateScore(tries, score),
       questions: restOfQuestions,
       modal: {
-        state: 'hide'
+        state: "hide"
       }
     });
   };
 
   updateScore(tries, score) {
     switch (tries) {
-      case 1: return score + 10;
-      case 2: return score + 5;
-      case 3: return score + 2;
-      default: return score + 1;
+      case 1:
+        return score + 10;
+      case 2:
+        return score + 5;
+      case 3:
+        return score + 2;
+      default:
+        return score + 1;
     }
   }
 
@@ -148,18 +150,29 @@ class QuizApp extends Component {
       ...this.getInitialState(this.props.totalQuestions)
     });
   };
-  begin = () =>{
-    this.setState({step:1})
-  }
+  begin = () => {
+    this.setState({ step: 1 });
+  };
   render() {
-    const { step, questions, userAnswers, totalQuestions, score, modal } = this.state;
-    if(step==0)
-    return(
-      <div className="start-container">
-        <img className="start-img"onClick={this.begin} src={require("../Img/logo.png")} />
-        {/* <a onClick={this.begin}>Start test</a> */}
-      </div>
-    )
+    const {
+      step,
+      questions,
+      userAnswers,
+      totalQuestions,
+      score,
+      modal
+    } = this.state;
+    if (step == 0)
+      return (
+        <div className="start-container">
+          <img
+            className="start-img"
+            onClick={this.begin}
+            src={require("../Img/logo.png")}
+          />
+          {/* <a onClick={this.begin}>Start test</a> */}
+        </div>
+      );
     else if (step >= totalQuestions + 1) {
       return (
         <Results
@@ -168,19 +181,20 @@ class QuizApp extends Component {
           userAnswers={userAnswers}
         />
       );
-    } else return (
-      <Fragment>
-        <Quiz
-          step={step}
-          questions={questions}
-          totalQuestions={totalQuestions}
-          score={score}
-          handleAnswerClick={this.handleAnswerClick}
-          handleEnterPress={this.handleEnterPress}
-        />
-        { modal.state === 'show' && <Modal modal={modal} /> }
-      </Fragment>
-    );
+    } else
+      return (
+        <Fragment>
+          <Quiz
+            step={step}
+            questions={questions}
+            totalQuestions={totalQuestions}
+            score={score}
+            handleAnswerClick={this.handleAnswerClick}
+            handleEnterPress={this.handleEnterPress}
+          />
+          {modal.state === "show" && <Modal modal={modal} />}
+        </Fragment>
+      );
   }
 }
 
