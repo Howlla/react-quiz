4a03c66ba5d590ee84d33485f0c815cdde4d39d8
Bhavye Sharma <bhavye.sharma1996@gmail.com>
2019-08-09 02:02:29 +0530

    Update quiz-data.js

--- src/data/quiz-data.js
+++ src/data/quiz-data.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 const QUESTION_DATA = [
   {
@@ -12,7 +12,11 @@ const QUESTION_DATA = [
     correct: 0
   },
   {
-    question: <span>Who is the highest run scorer in women's international cricket?</span>,
+    question: (
+      <span>
+        Who is the highest run scorer in women's international cricket?
+      </span>
+    ),
     answers: [
       <span>Anita Kanwar.</span>,
       <span>Taniya Bhatia.</span>,
@@ -52,7 +56,9 @@ const QUESTION_DATA = [
     correct: 0
   },
   {
-    question: <span>First female of Indian origin to climb mount everest?</span>,
+    question: (
+      <span>First female of Indian origin to climb mount everest?</span>
+    ),
     answers: [
       <span>Mary Kom.</span>,
       <span>Bachendri Pal.</span>,
@@ -62,7 +68,12 @@ const QUESTION_DATA = [
     correct: 1
   },
   {
-    question: <span>First female doctor in India and the first Indian woman to obtain a medical degree in the United States</span>,
+    question: (
+      <span>
+        First female doctor in India and the first Indian woman to obtain a
+        medical degree in the United States
+      </span>
+    ),
     answers: [
       <span>Dr. Rekha Saluja.</span>,
       <span>Dr. Padmavati.</span>,
@@ -72,38 +83,50 @@ const QUESTION_DATA = [
     correct: 2
   },
   {
-    question: <span>Women are at increased risk of <strong>Dry Eye </strong>compared to men<sup>1</sup><br/>
-    <code>what is the risk level?</code>
-    </span>,
+    question: (
+      <span>
+        What quality of Dimethyl Fumarate can make it best among DMTs for MS
+        <sup>3</sup>?
+      </span>
+    ),
     answers: [
-      <span>55%.</span>,
-      <span>77%.</span>,
-      <span>70%.</span>,
-      <span>90%.</span>
+      <span>Efficacy.</span>,
+      <span>Ease of formulation.</span>,
+      <span>Tolerability Profile.</span>,
+      <span>All the above.</span>
     ],
-    correct: 2
+    correct: 4,
+    isAll: true
   },
   {
-    question: <span>As per <strong>DEWS ll</strong> women are found to use more artificial tears as compared to men<sup>1</sup><br/>
-    <code>How much more do you think?</code></span>,
+    question: (
+      <span>
+        Can you guesstimate the most common symptom of Multiple Sclerosis in
+        Patients<sup>2</sup>?
+      </span>
+    ),
     answers: [
-      <span>15%.</span>,
-      <span>20%.</span>,
-      <span>25%.</span>,
-      <span>35%.</span>
+      <span>Walking Fatigue.</span>,
+      <span>Fatigue.</span>,
+      <span>Depression.</span>,
+      <span>Bladder and Bowel symptoms.</span>
+    ],
+    correct: 0
+  },
+  {
+    question: (
+      <span>
+        Can you estiamte the ratio of occurence of Multiple Sclerosis in Female
+        to Male patients<sup>1</sup>?
+      </span>
+    ),
+    answers: [
+      <span>1:1</span>,
+      <span>2:1.</span>,
+      <span>3:1.</span>,
+      <span>None of the above.</span>
     ],
     correct: 1
-  },
-  {
-    question: <span>Studies have shown women are more likely to present severe dry eyes<sup>1</sup><br/>
-      <code>What percentage of women experience it?</code> </span>,
-    answers: [
-      <span>12%.</span>,
-      <span>25%.</span>,
-      <span>30%.</span>,
-      <span>33%.</span>
-    ],
-    correct: 3
   }
 ];
 
