65132d875d8a2980e66909696519c3b41de82113
bhavyesharma <bhavye.sharma1996@gmail.com>
2019-08-13 00:05:42 +0530

    fireworks and modified questions

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import tally from '../helpers/tally';
+import { Fireworks } from 'fireworks/lib/react'
+
 
 const Results = ({ userAnswers, score, restartQuiz }) => {
   const triesTotal = tally(userAnswers);
@@ -9,18 +11,30 @@ const Results = ({ userAnswers, score, restartQuiz }) => {
   const threeTries = triesTotal[3] && <div><strong>{triesTotal[3]}</strong> on the third try.</div>;
   const fourTries = triesTotal[4] && <div><strong>{triesTotal[4]}</strong> on the fourth try.</div>;
   
+  let fxProps = {
+    count: 3,
+    interval: 1000,
+    colors: ['#ED3837', '#F2853B', '#373435'],
+    calc: (props, i) => ({
+      ...props,
+      x: (i + 1) * (window.innerWidth / 3) - (i + 1) * 100,
+      y: 200 + Math.random() * 100 - 50 + (i === 2 ? -80 : 0)
+    })
+  }
   return (
     <div className="results-container">
-      <h2>Quiz Results</h2>
+     <Fireworks {...fxProps} />
+      <h2>Congratulations! You have won!</h2>
       {/* <div>You answered...</div>
       {oneTry}
       {twoTries}
       {threeTries}
       {fourTries} */}
       <div className="results-total">Your Total Score is <strong>{score}</strong>.</div>
+      {<img src="https://i.imgur.com/ibj6WRr.png" alt="Packshot" height="300" opacity = "0.25" border="0"/>}
+
       <a onClick={restartQuiz}>Restart Quiz</a>
-      <img src="https://i.ibb.co/m0SQt1R/Refresh-Img.png" alt="Refresh" height="400" opacity="0.25"width="784"border="0"/>
-      {/* {<img src="https://i.ibb.co/WB7PZ6Y/last-page.png"/>} */}
+      <img src="https://i.ibb.co/m0SQt1R/Refresh-Img.png" alt="Refresh" height="200" opacity="0.25"border="0"/>
       {/* <img src="https://serving.photos.photobox.com/23013902e581cda029a09c1294bf34d6410349693975825a90b621845c522a07a4d2388d.jpg" alt="Refresh" height="382" width="784"/> */}
     </div>
   );
--- src/data/quiz-data.js
+++ src/data/quiz-data.js
@@ -1,77 +1,78 @@
 import React from "react";
 
 const QUESTION_DATA = [
+  // {
+  //   question: <span>Who is India's first female commercial pilot?</span>,
+  //   answers: [
+  //     <span>Durga Banerjee.</span>,
+  //     <span>Mary Kom.</span>,
+  //     <span>Mithali Raj.</span>,
+  //     <span>Amrita Sher-Gil.</span>
+  //   ],
+  //   correct: 0
+  // },
+  // {
+  //   question: (
+  //     <span>
+  //       Who is the highest run scorer in women's international cricket?
+  //     </span>
+  //   ),
+  //   answers: [
+  //     <span>Anita Kanwar.</span>,
+  //     <span>Taniya Bhatia.</span>,
+  //     <span>Mithali Raj.</span>,
+  //     <span>Tim Seifert.</span>
+  //   ],
+  //   correct: 2
+  // },
+  // {
+  //   question: <span>First woman to join Indian Police Services?</span>,
+  //   answers: [
+  //     <span>Meera Borwankar.</span>,
+  //     <span>Sonia Narang.</span>,
+  //     <span>Kiran Bedi.</span>,
+  //     <span>Lakshmi Sahgal.</span>
+  //   ],
+  //   correct: 2
+  // },
+  // {
+  //   question: <span>First female of Indian Origin to go to space?</span>,
+  //   answers: [
+  //     <span>Sunita Williams.</span>,
+  //     <span>Sunita Malhotra.</span>,
+  //     <span>Ravisha Raina.</span>,
+  //     <span>Kalpana Chawla.</span>
+  //   ],
+  //   correct: 3
+  // },
+  // {
+  //   question: <span>First female judge of Indian Supreme Court </span>,
+  //   answers: [
+  //     <span>M. Fathima Beevi.</span>,
+  //     <span>Sheila Dixit.</span>,
+  //     <span>Uma Bharti.</span>,
+  //     <span>Mamta Banerjee.</span>
+  //   ],
+  //   correct: 0
+  // },
   {
-    question: <span>Who is India's first female commercial pilot?</span>,
+    question: (
+      <span> How do you think in India female patients are placed versus male patients in terms of treatment?</span>
+    ),
     answers: [
-      <span>Durga Banerjee.</span>,
-      <span>Mary Kom.</span>,
-      <span>Mithali Raj.</span>,
-      <span>Amrita Sher-Gil.</span>
+      <span>Lower than Male patients.</span>,
+      <span>Higher than Male patients.</span>,
+      <span>Equal to Male patients.</span>,
+      <span> None of the above.</span>
     ],
-    correct: 0
+    correct: 1,
+   isAll:true
   },
   {
     question: (
       <span>
-        Who is the highest run scorer in women's international cricket?
-      </span>
-    ),
-    answers: [
-      <span>Anita Kanwar.</span>,
-      <span>Taniya Bhatia.</span>,
-      <span>Mithali Raj.</span>,
-      <span>Tim Seifert.</span>
-    ],
-    correct: 2
-  },
-  {
-    question: <span>First woman to join Indian Police Services?</span>,
-    answers: [
-      <span>Meera Borwankar.</span>,
-      <span>Sonia Narang.</span>,
-      <span>Kiran Bedi.</span>,
-      <span>Lakshmi Sahgal.</span>
-    ],
-    correct: 2
-  },
-  {
-    question: <span>First female of Indian Origin to go to space?</span>,
-    answers: [
-      <span>Sunita Williams.</span>,
-      <span>Sunita Malhotra.</span>,
-      <span>Ravisha Raina.</span>,
-      <span>Kalpana Chawla.</span>
-    ],
-    correct: 3
-  },
-  {
-    question: <span>First female judge of Indian Supreme Court </span>,
-    answers: [
-      <span>M. Fathima Beevi.</span>,
-      <span>Sheila Dixit.</span>,
-      <span>Uma Bharti.</span>,
-      <span>Mamta Banerjee.</span>
-    ],
-    correct: 0
-  },
-  {
-    question: (
-      <span>First female of Indian origin to climb mount everest?</span>
-    ),
-    answers: [
-      <span>Mary Kom.</span>,
-      <span>Bachendri Pal.</span>,
-      <span>Aditi Chauhan.</span>,
-      <span>Kalpana Chawla.</span>
-    ],
-    correct: 1
-  },
-  {
-    question: (
-      <span>
-        First female doctor in India and the first Indian woman to obtain a
-        medical degree in the United States
+        Who is the First female doctor in India and the first Indian woman to obtain a
+        medical degree in the United States?
       </span>
     ),
     answers: [
@@ -85,7 +86,7 @@ const QUESTION_DATA = [
   {
     question: (
       <span>
-        What quality of Dimethyl Fumarate can make it best among DMTs for MS
+        What according to you makes Dimethyl Fumarate a better DMT than other available DMTs?
         <sup>3</sup>?
       </span>
     ),
@@ -116,8 +117,7 @@ const QUESTION_DATA = [
   {
     question: (
       <span>
-        Can you estiamte the ratio of occurence of Multiple Sclerosis in Female
-        to Male patients<sup>1</sup>?
+        Can you estimate the ratio of female vs male patients of Multiple Sclerosis?<sup>1</sup>?
       </span>
     ),
     answers: [
