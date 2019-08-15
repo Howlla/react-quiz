86f16f5a1af1c9861c06d9dea5205718ebc07614
Bhavye Sharma <bhavye.sharma1996@gmail.com>
2019-08-15 19:27:14 +0530

    Update shuffleQuestions.js

--- src/helpers/shuffleQuestions.js
+++ src/helpers/shuffleQuestions.js
@@ -1,5 +1,6 @@
 const shuffleQuestions = (arr) => {
-  return arr.sort((a,b) => Math.random() < .5 ? 1 : -1);
+//   return arr.sort((a,b) => Math.random() < .5 ? 1 : -1);
+  return arr
 };
 
 export default shuffleQuestions;
